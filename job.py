ea8809094d37f759c3163299fa3217c8c1581660
Vaagen <pcthrane@stud.ntnu.no>
2019-03-20 16:49:55 +0100

    Backlit and fix .loc calls in S4_wrapper.

--- job.py
+++ job.py
@@ -8,6 +8,8 @@ def main(tot_num_processes=1, process_num=1):
     pd.options.mode.chained_assignment = None  # default='warn'
     output_filename = 'output/test'
     df = setup_simulation()
+    print(df.head())
+    print(df.tail())
     if(tot_num_processes == 1):
         df = run_sim(df)
         # df = run_sim_intermediate_output(df,indexes=None,divisions=10, output_filename=output_filename)
@@ -21,37 +23,39 @@ def setup_simulation():
     NumBasis =100# Comp time ~ NumBasis^3, Memory used ~ NumBasis^2
     # Use length micrometers
     # Wavelength
-    wl = 0.85
+    wl = 0.915
     # Height of pillar
-    z_Pillar = 0.65
+    z_Pillar = 0.715
     # Pillar orientation
-    theta = 0 # Degrees
+    theta = 0.0 # Degrees
     # Epsilon
-    epsilon_a_Si = 3.56**2. #3.7+0.02j
-    epsilon_fused_SiO2 = 1.45**2.
+    # epsilon_a_Si = 3.56**2.
+    # epsilon_fused_SiO2 = 1.45**2.
+    epsilon_a_Si = 13.921350491788342+0.17024349284921841j
+    epsilon_fused_SiO2 = 2.121673706817246
     # Incoming amplitudes
-    sAmplitude = 1
+    sAmplitude = 1.0
     pAmplitude = 1e-15
     # Incoming angles
     theta_incidence = 1e-15 # Polar angle in degrees
     phi_incidence   = 1e-15 # Azimuthal angle in degrees
     ################################## Geometries
-    x_steps = 10
-    y_steps = 10
-    x_start = 0.05
-    y_start = 0.05
-    x_stop  = 0.4
-    y_stop  = 0.4
+    x_steps = 76
+    y_steps = 76
+    x_start = 0.065 #0.05
+    y_start = 0.065 #0.05
+    x_stop  = 0.44 #0.4
+    y_stop  = 0.44 #0.4
     ################################## Unit cell
     # Size of cell
-    a = 0.48
+    a = 0.65 #0.48
     b = a # Length of second lattice vector
     # # Rectangular basis
-    angle_basis_vectors = np.pi/180 * 90
-    angle_shift_basis   = np.pi/180 * 0
-    # # Hexagonal basis
-    # angle_basis_vectors = np.pi/180 * 120
+    # angle_basis_vectors = np.pi/180 * 90
     # angle_shift_basis   = np.pi/180 * 0
+    # # Hexagonal basis
+    angle_basis_vectors = np.pi/180 * 120
+    angle_shift_basis   = np.pi/180 * 0
     ####################################################################
     x_values = np.linspace(x_start,x_stop, x_steps)
     y_values = np.linspace(y_start,y_stop, y_steps)
@@ -66,7 +70,6 @@ def setup_simulation():
         'tss_0':np.nan,
         'rss_0':np.nan,
         'phi_tss_0':np.nan})
-
     return df
 
 if __name__ == '__main__':
