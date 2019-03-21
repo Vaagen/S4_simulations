0faf73e166351fd9b58a83501af82c0efbdc7aad
Vaagen <pcthrane@stud.ntnu.no>
2019-03-21 17:01:19 +0100

    Add ploting of fields. Move setup_simulation().

--- S4_wrapper.py
+++ S4_wrapper.py
@@ -7,6 +7,60 @@ import itertools
 
 import time
 
+def setup_simulation(NumBasis, x_steps, y_steps):
+    # output_filename = 'output/arbabi_rectangle.h5'
+    NumBasis =NumBasis# Comp time ~ NumBasis^3, Memory used ~ NumBasis^2
+    # Use length micrometers
+    # Wavelength
+    wl = 0.915
+    # Height of pillar
+    z_Pillar = 0.715
+    # Pillar orientation
+    theta = 0.0 # Degrees
+    # Epsilon
+    # epsilon_a_Si = 3.56**2.
+    # epsilon_fused_SiO2 = 1.45**2.
+    epsilon_a_Si = 13.921350491788342+0.17024349284921841j
+    epsilon_fused_SiO2 = 2.121673706817246
+    # Incoming amplitudes
+    sAmplitude = 1.0
+    pAmplitude = 1e-15
+    # Incoming angles
+    theta_incidence = 1e-15 # Polar angle in degrees
+    phi_incidence   = 1e-15 # Azimuthal angle in degrees
+    ################################## Geometries
+    x_steps = x_steps
+    y_steps = y_steps
+    x_start = 0.13 #0.08 #0.065 #0.05
+    y_start = 0.39 #0.375 #0.065 #0.05
+    x_stop  = 0.13 #0.08 #0.065 #0.44 #0.4
+    y_stop  = 0.39 #0.375 #0.44 #0.4
+    ################################## Unit cell
+    # Size of cell
+    a = 0.65 #0.48
+    b = a # Length of second lattice vector
+    # # Rectangular basis
+    # angle_basis_vectors = np.pi/180 * 90
+    # angle_shift_basis   = np.pi/180 * 0
+    # # Hexagonal basis
+    angle_basis_vectors = np.pi/180 * 120
+    angle_shift_basis   = np.pi/180 * 0
+    ####################################################################
+    x_values = np.linspace(x_start,x_stop, x_steps)
+    y_values = np.linspace(y_start,y_stop, y_steps)
+    x_mesh, y_mesh = np.meshgrid(x_values, y_values)
+    x_mesh = np.ndarray.flatten(x_mesh)
+    y_mesh = np.ndarray.flatten(y_mesh)
+
+    df = pd.DataFrame(data={'NumBasis':NumBasis, 'Dx':x_mesh, 'Dy':y_mesh, 'z_Pillar':z_Pillar,
+        'theta':theta,'wl':wl,'a':a,'b':b,'angle_basis_vectors':angle_basis_vectors,'angle_shift_basis':angle_shift_basis,
+        'epsilon_a_Si':epsilon_a_Si,'epsilon_fused_SiO2':epsilon_fused_SiO2,
+        'sAmplitude':sAmplitude,'pAmplitude':pAmplitude,'theta_incidence':theta_incidence,'phi_incidence':phi_incidence,
+        'tss_0':np.nan,
+        'rss_0':np.nan,
+        'phi_tss_0':np.nan})
+    return df
+
 def run_sim(df, indexes=np.array([])):
     '''
     Run simulation for a set of different Dx, Dy, note that all other
@@ -79,6 +133,7 @@ def run_sim(df, indexes=np.array([])):
             df.iloc[i, df.columns.get_loc('tss_0')] = np.abs(forw_Amp_Air[0]/forw_Amp_Substrate[0])
             df.iloc[i, df.columns.get_loc('rss_0')] = np.abs(back_Amp_Substrate[0]/forw_Amp_Substrate[0])
             df.iloc[i, df.columns.get_loc('phi_tss_0')] = np.angle(forw_Amp_Substrate[0]/np.sqrt(epsilon_fused_SiO2)/forw_Amp_Air[0])
+    S.GetFieldsOnGrid(z = 0.2, NumSamples=(4,4), Format = 'FileWrite', BaseFilename = 'field')
     return df
 
 def run_sim_parallel(df,tot_num_processes=1, process_num=1, output_filename = 'output/test'):
@@ -91,21 +146,59 @@ def run_sim_parallel(df,tot_num_processes=1, process_num=1, output_filename = 'o
     df_out = run_sim(df_out)
     df_out.to_hdf(output_filename +'.h5', key='df', append=True)
 
-
-def run_sim_intermediate_output(df, indexes=None, divisions=10, output_filename='output/test.h5'):
-    seconds_start = time.time()
-    if indexes==None:
-        indexes = np.arange(df.index.size)
-    if(len(indexes)<divisions):
-        divisions = len(indexes)
-    indexes_array = np.array_split(indexes,divisions)
-    for i in range(divisions):
-        df = run_sim(df,indexes_array[i])
-        df_out = df.iloc[indexes_array[i][0]:indexes_array[i][-1]+1]
-        df_out.to_hdf(output_filename, key='df', append=True)
-        seconds = time.time()-seconds_start
-        hours   = np.floor(seconds/3600)
-        minutes = np.floor((seconds - hours*3600)/60)
-        seconds = seconds - hours*3600 - minutes*60
-        print('{:.1f}% finished. \t{:.0f} h, {:.0f} min, {:.0f} sec.'.format((i+1)/divisions*100, hours, minutes, seconds))
-    return df
+def get_fields(df, x,y,z, index=0):
+    '''
+    Gives fields at locations x, y and z,
+    after solving with S4 for the case in df with index=index.
+    x, y and z should be numpy lists of same length.
+    '''
+    a1 = [df.iloc[index, df.columns.get_loc('a')],0]
+    a2 = [df.iloc[index, df.columns.get_loc('b')]*np.cos(df.iloc[index, df.columns.get_loc('angle_basis_vectors')]), df.iloc[index, df.columns.get_loc('b')]*np.sin(df.iloc[index, df.columns.get_loc('angle_basis_vectors')])]
+    b1 = [0,0]
+    b2 = [0,0]
+    b1[0] = np.cos(df.iloc[index, df.columns.get_loc('angle_shift_basis')])*a1[0] - np.sin(df.iloc[index, df.columns.get_loc('angle_shift_basis')])*a1[1]
+    b1[1] = np.cos(df.iloc[index, df.columns.get_loc('angle_shift_basis')])*a1[1] + np.sin(df.iloc[index, df.columns.get_loc('angle_shift_basis')])*a1[0]
+    b2[0] = np.cos(df.iloc[index, df.columns.get_loc('angle_shift_basis')])*a2[0] - np.sin(df.iloc[index, df.columns.get_loc('angle_shift_basis')])*a2[1]
+    b2[1] = np.cos(df.iloc[index, df.columns.get_loc('angle_shift_basis')])*a2[1] + np.sin(df.iloc[index, df.columns.get_loc('angle_shift_basis')])*a2[0]
+    S = S4.New(Lattice=((b1[0],b1[1]),(b2[0],b2[1])), NumBasis=int(df.iloc[index, df.columns.get_loc('NumBasis')]))
+    ####################### Options
+    S.SetOptions(
+        Verbosity = 0,
+        LatticeTruncation = 'Circular', # Circular
+        DiscretizedEpsilon = False, # False
+        DiscretizationResolution = 8, # 8
+        PolarizationDecomposition = True, # False
+        PolarizationBasis = 'Jones', # Default
+        LanczosSmoothing = False, # False
+        SubpixelSmoothing = False, # False
+        ConserveMemory = False # False
+    )
+    ####################### Setup material and exitation.
+    S.SetMaterial( Name='Air', Epsilon=1)
+    S.SetMaterial( Name='a_Si', Epsilon=df.iloc[index, df.columns.get_loc('epsilon_a_Si')])
+    S.SetMaterial( Name='fused_SiO2', Epsilon=df.iloc[index, df.columns.get_loc('epsilon_fused_SiO2')])
+    # Front illuminated: Illuminated from air
+    # S.AddLayer(Name = 'AirAbove', Thickness = 0, Material = 'Air')
+    # S.AddLayer(Name = 'MetaLayer', Thickness = df.iloc[0, df.columns.get_loc('z_Pillar')], Material = 'Air')
+    # S.AddLayer(Name = 'Substrate', Thickness = 0, Material = 'fused_SiO2')
+    # Back illuminated: Illuminated from substrate
+    S.AddLayer(Name = 'Substrate', Thickness = 0, Material = 'fused_SiO2')
+    S.AddLayer(Name = 'MetaLayer', Thickness = df.iloc[index, df.columns.get_loc('z_Pillar')], Material = 'Air')
+    S.AddLayer(Name = 'AirAbove', Thickness = 0, Material = 'Air')
+    #
+    S.SetExcitationPlanewave(IncidenceAngles=(df.iloc[index, df.columns.get_loc('theta_incidence')],df.iloc[index, df.columns.get_loc('phi_incidence')]),
+        sAmplitude=df.iloc[index, df.columns.get_loc('sAmplitude')], pAmplitude=df.iloc[index, df.columns.get_loc('pAmplitude')], Order=0)
+    S.SetFrequency(1/df.iloc[index, df.columns.get_loc('wl')])
+    Glist = S.GetBasisSet()
+    num_modes = len(Glist)
+    Dx = df.iloc[index, df.columns.get_loc('Dx')]
+    Dy = df.iloc[index, df.columns.get_loc('Dy')]
+    ###################################################################
+    S.SetRegionEllipse(Layer='MetaLayer', Material='a_Si', Center=(0,0), Angle=df.iloc[index,df.columns.get_loc('theta')], Halfwidths=(Dx/2.0,Dy/2.0))
+    # S.SetRegionRectangle(Layer='MetaLayer', Material='a_Si', Center=(0,0), Angle=df.iloc[index,df.columns.get_loc('theta')], Halfwidths=(Dx/2.0,Dy/2.0))
+    ###################################################################
+    E = np.zeros((len(x),3), dtype=np.complex)
+    H = np.zeros((len(x),3), dtype=np.complex)
+    for i in range(len(x)):
+        (E[i],H[i]) = S.GetFields(x[i],y[i],z[i])
+    return E,H
--- job.py
+++ job.py
@@ -2,75 +2,29 @@ import numpy as np
 import pandas as pd
 import S4
 from S4_wrapper import *
+from S4_plotter import *
 import argparse
 
 def main(tot_num_processes=1, process_num=1):
-    pd.options.mode.chained_assignment = None  # default='warn'
+    NumBasis = 300
+    x_steps = 1
+    y_steps = 1
     output_filename = 'output/test'
-    df = setup_simulation()
-    print(df.head())
-    print(df.tail())
-    if(tot_num_processes == 1):
-        df = run_sim(df)
-        # df = run_sim_intermediate_output(df,indexes=None,divisions=10, output_filename=output_filename)
-        df.to_hdf(output_filename +'.h5', key='df', append=True)
-        # print(df.loc[:,['tss_0','rss_0','phi_tss_0']])
-    else:
-        run_sim_parallel(df,tot_num_processes=tot_num_processes, process_num=process_num, output_filename = output_filename +'_'+ str(process_num))
+    df = setup_simulation(NumBasis, x_steps, y_steps)
+    ###################################################
+    plot_fields(df, 0, 100)
 
-def setup_simulation():
-    # output_filename = 'output/arbabi_rectangle.h5'
-    NumBasis =100# Comp time ~ NumBasis^3, Memory used ~ NumBasis^2
-    # Use length micrometers
-    # Wavelength
-    wl = 0.915
-    # Height of pillar
-    z_Pillar = 0.715
-    # Pillar orientation
-    theta = 0.0 # Degrees
-    # Epsilon
-    # epsilon_a_Si = 3.56**2.
-    # epsilon_fused_SiO2 = 1.45**2.
-    epsilon_a_Si = 13.921350491788342+0.17024349284921841j
-    epsilon_fused_SiO2 = 2.121673706817246
-    # Incoming amplitudes
-    sAmplitude = 1.0
-    pAmplitude = 1e-15
-    # Incoming angles
-    theta_incidence = 1e-15 # Polar angle in degrees
-    phi_incidence   = 1e-15 # Azimuthal angle in degrees
-    ################################## Geometries
-    x_steps = 76
-    y_steps = 76
-    x_start = 0.065 #0.05
-    y_start = 0.065 #0.05
-    x_stop  = 0.44 #0.4
-    y_stop  = 0.44 #0.4
-    ################################## Unit cell
-    # Size of cell
-    a = 0.65 #0.48
-    b = a # Length of second lattice vector
-    # # Rectangular basis
-    # angle_basis_vectors = np.pi/180 * 90
-    # angle_shift_basis   = np.pi/180 * 0
-    # # Hexagonal basis
-    angle_basis_vectors = np.pi/180 * 120
-    angle_shift_basis   = np.pi/180 * 0
-    ####################################################################
-    x_values = np.linspace(x_start,x_stop, x_steps)
-    y_values = np.linspace(y_start,y_stop, y_steps)
-    x_mesh, y_mesh = np.meshgrid(x_values, y_values)
-    x_mesh = np.ndarray.flatten(x_mesh)
-    y_mesh = np.ndarray.flatten(y_mesh)
-
-    df = pd.DataFrame(data={'NumBasis':NumBasis, 'Dx':x_mesh, 'Dy':y_mesh, 'z_Pillar':z_Pillar,
-        'theta':theta,'wl':wl,'a':a,'b':b,'angle_basis_vectors':angle_basis_vectors,'angle_shift_basis':angle_shift_basis,
-        'epsilon_a_Si':epsilon_a_Si,'epsilon_fused_SiO2':epsilon_fused_SiO2,
-        'sAmplitude':sAmplitude,'pAmplitude':pAmplitude,'theta_incidence':theta_incidence,'phi_incidence':phi_incidence,
-        'tss_0':np.nan,
-        'rss_0':np.nan,
-        'phi_tss_0':np.nan})
-    return df
+    # ###################################################
+    # pd.options.mode.chained_assignment = None  # default='warn'
+    # if(tot_num_processes == 1):
+    #     df = run_sim(df)
+    #     # df = run_sim_intermediate_output(df,indexes=None,divisions=10, output_filename=output_filename)
+    #     df.to_hdf(output_filename +'.h5', key='df', append=True)
+    #     # print(df.loc[:,['tss_0','rss_0','phi_tss_0']])
+    #     print(df.loc[0,'NumBasis'])
+    #     print(df.loc[0,'tss_0']**2*np.sqrt(df.loc[:,'epsilon_fused_SiO2']))
+    # else:
+    #     run_sim_parallel(df,tot_num_processes=tot_num_processes, process_num=process_num, output_filename = output_filename +'_'+ str(process_num))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
